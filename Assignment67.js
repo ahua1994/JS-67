67cc41467db13798f40d6e3ffe079ab4437a034e
Andy Hua <user.andy.hua@gmail.com>
2022-09-29 22:43:32 -0700

    fixed card style

--- Assignment67.js
+++ Assignment67.js
@@ -30,22 +30,21 @@ async function displayProducts() {
     // 8. generate a html card for products
     let products = ``;
     data.forEach((product) => {
-        products += `<div class="col">
-        <div class="col-4">
-          <div class="card">
-          <img src=${product.image} class="card-img-top" alt="pic">
-            <div class="card-body">
-              <h5 class="card-title">${product.title}</h5>
-              <h5 class="card-title">$ ${product.price}</h5>
-              <p class="card-text">${product.description}</p> 
-            </div>
-          </div>
-        </div>`;
+        products += `<div class="col-4">
+                        <div class="card" style="height: 100%">
+                            <img src=${product.image} class="card-img-top" alt="pic" style="height:450px">
+                            <div class="card-body">
+                                <h5 class="card-title">${product.title}</h5>
+                                <h5 class="card-title">$ ${product.price}</h5>
+                                <p class="card-text">${product.description}</p> 
+                            </div>
+                        </div>    
+                    </div> `;
     });
 
     // 9. generate a card container to display all of the products
     let cardGroup = document.createElement("div");
-    cardGroup.classList.add("card-group", "w-100", "row");
+    cardGroup.classList.add("row");
     document.body.prepend(cardGroup);
     document.body.classList.add("w-100");
     cardGroup.innerHTML = products;
